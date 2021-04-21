73241518f66c9588b5ae07314ca8d19e62500b9d
Dario Tedeschi <dario.tedeschi86@hotmail.it>
2021-04-21 11:29:20 +0200

    completo

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ var app = new Vue ({
         headNav: ["Solution", 
                   "Pricing",
                   "Blog",
-                  "Sign In"]
+                  "Sign In"],
+        product: ["Feedback", "Surveys", "Review", "Praise", "SLack integration", "Schedule a demo", "Pricing"],
+        labs: ["Feedback", "Surveys", "Review", "Praise"],
+        about:["Feedback", "Surveys", "Review", "Praise"],
+        connect:["Feedback", "Surveys", "Review", "Praise"],
+        fromTheBlog:["Feedback", "Surveys", "Review", "Praise"],
+                
     }
 })
